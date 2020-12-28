6f785789250276d30d91f5dc56b34a067e6e1fcf
Nathan Bunker <nbunker@immregistries.org>
2020-12-28 12:58:19 -0700

    Updated version

--- src/main/java/org/immregistries/puente/SoftwareVersion.java
+++ src/main/java/org/immregistries/puente/SoftwareVersion.java
@@ -2,6 +2,6 @@ package org.immregistries.puente;
 
 public class SoftwareVersion {
 
-  public static final String VERSION = "0.3";
-  public static final String RELEASE_DATE = "21 Dec 2020";
+  public static final String VERSION = "0.4";
+  public static final String RELEASE_DATE = "28 Dec 2020";
 }
